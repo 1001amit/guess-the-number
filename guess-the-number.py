cd832086a7805c188318124c670850b834cc5f5b
WHITEDEVIL <amit12589630@gmail.com>
2024-07-03 18:56:50 +0530

    Update guess-the-number.py

--- guess-the-number.py
+++ guess-the-number.py
@@ -4,3 +4,15 @@ def guess_the_number():
     number_to_guess = random.randint(1, 100)
     attempts = 0
 
+print("welcome to the guess number game")
+print("I have chosen a number between 1 to 100")
+
+while True:
+    user_guess= input("Enter your guess: ")
+
+    try:
+        user_guess= int(user_guess)
+    except ValueError:
+        print("enter a valid no.")
+        continue
+    
